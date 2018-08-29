0a337a5b97759203da4fc82f6c10ac6572fc12e9
Jakub Mazur <jakub.mazur@wingu.de>
2018-08-29 16:33:35 +0200

    initial commit

--- WinguDemo/AppDelegate.swift
+++ WinguDemo/AppDelegate.swift
@@ -7,40 +7,44 @@
 //
 
 import UIKit
+import winguSDKEssential
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    
+    lazy var winguLocations: WinguLocations = {
+        let winguLocations = WinguLocations.shared
+        winguLocations.apiKey = <# Your API KEY #>
+        winguLocations.delegate = self
+        winguLocations.beaconScanner.rediscoverNotificationTime = 0             // Set this to 0 if you want notification every time user pass channel (spamming avoiding time)
+        winguLocations.beaconScanner.beaconFetchDataTime = 60                   // 60 seconds to hold cached data, if you want you may also trigger update(:) manually.
+        winguLocations.returnOnlyChanelsWithContent = true                      // This will return only beacons with contents attached
+        NotificationsManager.shared.shouldSendWinguNotifications = true         // If you've notifications enabled you will get wingu notifications.
+        NotificationsManager.shared.onlyNotificationsWithContentUpdate = true   // Change it to false if you're interested in notification whenever content will update and user pass beacon next time
+        return winguLocations
+    }()
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        winguLocations.useLaunchOptions(launchOptions)
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        winguLocations.start()
+        NotificationsManager.requestNotificationsPermission()
+        return true
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
+extension AppDelegate: WinguLocationsDelegate {
+    func winguChannels(_ channels: [Channel]) {
+        UIApplication.shared.applicationIconBadgeNumber = channels.count
+        for channel in channels {
+            print(channel.content?.pack?.deck?.title ?? "-")
+            print(channel.content?.lastChange ?? "-")
+        }
+    }
+}
